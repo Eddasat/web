eca3b6d5047d3ec31cd773343c351d417b69fb84
Eddasat <1074923471@qq.com>
2018-12-09 00:03:30 +0800

    弹出层案例代码

--- js/弹出层.js
+++ js/弹出层.js
@@ -1,4 +1,4 @@
-$(document).ready(function($) {
+$(document).ready(function ($) {
     // 登录链接事件
     // $("#loginLink").click(function(){
     // 	// 显示弹出层遮罩
@@ -50,5 +50,38 @@ $(document).ready(function($) {
         });
     });
 
+    // 弹出层关闭回调函数
+    function closeCallback() {
+        $(".error-msg").html("");
+    }
+
+    // 显示弹出层
+    function showLayer(html, width, height, closeCallback) {
+        // 显示弹出层遮罩
+        $("#layer-mask").show();
+        // 显示弹出层窗体
+        $("#layer-pop").show();
+        // 设置弹出层窗体样式
+        $("#layer-pop").css({
+            width: width,
+            height: height
+        });
+        // 填充弹出层窗体内容
+        $("#layer-content").html(html);
+        // 弹出层关闭按钮绑定事件
+        $("#layer-close").click(function () {
+            // 弹出层关闭
+            hideLayer();
+            // 关闭的回调函数
+            closeCallback();
+        });
+    }
+
+    // 隐藏弹出层
+    function hideLayer() {
+        // 弹出层关闭
+        $("#layer-mask").hide();
+        $("#layer-pop").hide();
+    }
 
 });
