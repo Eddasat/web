8d1aabb9e84c8a7736403e5bcdfeab79921b9830
Eddasat <1074923471@qq.com>
2019-04-25 23:59:13 +0800

    es6

---
+++ ES6基础/ES6变量的解构赋值/解构/3-3/main.js
@@ -0,0 +1,14 @@
+// 字符串的结构赋值
+const str = 'I am the bone of my sword'; // 我是剑骨头
+
+// const [ a, b ,c, ...oth ] = str;
+
+// const [ ...spStr1 ] = str;
+// const spStr2 = str.split('');
+// const spStr3 = [ ...str ];
+
+// ------------------------------------------
+
+// 提取属性
+
+// const { length, split } = str;
---
+++ ES6基础/ES6变量的解构赋值/解构/3-4/main.js
@@ -0,0 +1,32 @@
+// 函数参数的解构赋值
+
+// function swap([x, y]) {
+// 	return [y, x];
+// };
+
+// let arr = [1, 2];
+// arr = swap(arr);
+
+function Computer({
+	cpu,
+	memory,
+	software = ['ie6'],
+	OS = 'windows 3.5'
+}) {
+
+	console.log(cpu);
+	console.log(memory);
+	console.log(software);
+	console.log(OS);
+
+};
+
+new Computer({
+	memory: '128G',
+	cpu: '80286',
+	OS: 'windows 10'
+});
+// 数值与布尔值的解构赋值
+
+const { valueOf: vo } = 1;
+const { toString: ts } = false;
