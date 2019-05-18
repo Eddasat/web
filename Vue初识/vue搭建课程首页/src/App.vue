7682921417befc36a27a197665ecb193c994dcb1
Eddasat <1074923471@qq.com>
2019-05-18 23:59:30 +0800

    es6

---
+++ Vue初识/vue搭建课程首页/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style>
+*{
+  margin: 0;
+  padding: 0;
+  list-style: none;
+}
+a{
+	text-decoration: none;
+}
+</style>
+
+
---
+++ Vue初识/vue搭建课程首页/src/components/header/index.vue
@@ -0,0 +1,35 @@
+<template>
+	<div class="c-header">
+		<ul class="header-ul">
+			<li>推荐</li><li>课程</li><li>实战</li><li>职业路线</li><li class="header-search"><img src="../../assets/search.png"></li><li class="header-history"><img src="../../assets/history.png" /></li>
+		</ul>
+	</div>
+</template>
+<style scoped>
+	.c-header{
+		position: fixed;
+		z-index: 3;
+		width: 375px;
+		top: 0;
+		background: #fff;
+		box-shadow: 0 2px 4px 0 rgba(0,0,0,0.10);
+	}
+	.header-ul{
+		font-size: 0;
+	}
+	.header-ul li{
+		display: inline-block;
+		padding-left: 20px;
+		font-size: 16px;
+		color: #71777D;
+		height: 44px;
+		line-height: 44px;
+	}
+	
+	.header-ul img{
+		width: 18px;
+	}
+	.header-ul .header-search{
+		padding-left: 60px;
+	}
+</style>
