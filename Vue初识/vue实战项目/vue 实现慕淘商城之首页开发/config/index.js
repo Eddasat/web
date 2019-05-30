632e4d070ba686ffb6fab968c5c2c35cf82ab3bc
Eddasat <1074923471@qq.com>
2019-05-31 00:00:47 +0800

    Update index.js

--- Vue初识/vue实战项目/vue 实现慕淘商城之首页开发/config/index.js
+++ Vue初识/vue实战项目/vue 实现慕淘商城之首页开发/config/index.js
@@ -10,7 +10,7 @@ module.exports = {
     // Paths
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
-    proxyTable: {},
+ 
 
     // Various Dev Server settings
     host: '0.0.0.0', // can be overwritten by process.env.HOST
